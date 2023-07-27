6df1c76d8d52c8766775dc93cdbcf00c724a3831
Abzu-Labs <jake@abzu.dev>
2023-07-27 10:08:06 -0500

    corrected main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,7 @@
 # main.py
-
 from fastapi import FastAPI
 from app.routes import router as api_router
 
 app = FastAPI()
 
-# Initialize clients
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-app.include_router(router)
+app.include_router(api_router, prefix="/api")
